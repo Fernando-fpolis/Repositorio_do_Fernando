8c5537ab616449995dc5d82321bddf52986b5463
Fernando-fpolis <153782147+Fernando-fpolis@users.noreply.github.com>
2024-01-30 00:05:54 -0300

    h

---
+++ assets/main.js
@@ -0,0 +1,6 @@
+const input_email = document.getElementById('input_email')
+// input_email.value => valor de email digitado
+//descrever bem individual, assim fica o Id para vincular na Javascript
+const elementos = document.getElementsByClassName('custom_input')
+//vai puxar todos os elementos, mas é uma interação pouco usado. Só se for para sumir com tudo.
+//personalizar e estilizar com class no CSS
